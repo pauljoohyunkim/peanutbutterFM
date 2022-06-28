f06703864888f5041dd8c765d0435ebc26e04f96
pbjk <pauljoohyunkim@gmail.com>
2022-06-28 22:35:00 +0200

    MD5

--- filelib/hasher.py
+++ filelib/hasher.py
@@ -8,3 +8,12 @@ def sha256sum(filepath, block=2**32):
             hasher.update(read_bytes)
             read_bytes = file.read(block)
     return hasher.hexdigest()
+
+def md5sum(filepath, block=2**32):
+    hasher = hashlib.md5()
+    with open(filepath, "rb") as file:
+        read_bytes = file.read(block)
+        while read_bytes:
+            hasher.update(read_bytes)
+            read_bytes = file.read(block)
+    return hasher.hexdigest()
