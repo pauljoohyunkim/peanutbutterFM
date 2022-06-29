bee775dfe12368acdce63dc7a809187dc17d13ad
pauljoohyunkim <pauljoohyunkim@gmail.com>
2022-06-29 02:26:30 +0200

    Function support for listing engine

--- listingenginelib/listingengine.py
+++ listingenginelib/listingengine.py
@@ -1,9 +1,11 @@
 revdict = lambda injectiveDictionary: {val:key for key,val in injectiveDictionary.items()}
 
+# funinvfun = (f, f^{-1})       (Function and its inverse)
 class ListingMap():
-    def __init__(self, listingDictionary={}):
+    def __init__(self, listingDictionary={}, funinvfun=(None,None)):
         self.listingDictionary = listingDictionary
         self.inverseDictionary = revdict(self.listingDictionary)
+        self.f, self.invf = funinvfun
     
     def aliases(self):
         return list(self.listingDictionary.values())
@@ -12,13 +14,17 @@ class ListingMap():
         return list(self.listingDictionary.keys())
 
     def eval(self, real):
-        if real in self.listingDictionary.keys():
+        if self.f != None:
+            return self.f(real)
+        elif real in self.listingDictionary.keys():
             return self.listingDictionary[real]
         else:
             return real
     
     def inveval(self, alias):
-        if alias in self.inverseDictionary.keys():
+        if self.invf != None:
+            return self.invf(alias)
+        elif alias in self.inverseDictionary.keys():
             return self.inverseDictionary[alias]
         else:
             return alias
