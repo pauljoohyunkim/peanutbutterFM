8c055128558abb7a041ffe83300089a2cb8c1e76
pauljoohyunkim <pauljoohyunkim@gmail.com>
2022-07-02 17:48:11 +0200

    Plugin Dictionary

--- pbPlugin/plugin.py
+++ pbPlugin/plugin.py
@@ -1,3 +1,8 @@
+# This is a dictionary of the form {PluginName: PluginObject} to keep track of which plugins there are.
+pluginDictionary = {}
+
 class Plugin():
     def __init__(self, name):
+        global pluginDictionary
         self.name = name
+        pluginDictionary[name] = self
